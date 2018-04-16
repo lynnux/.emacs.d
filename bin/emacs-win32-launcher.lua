b242d02331d4c30393581db0863b1abc6235850d
lynnux <lynnux@qq.com>
2018-04-16 19:24:09 +0800

    update for home

--- bin/emacs-win32-launcher.lua
+++ bin/emacs-win32-launcher.lua
@@ -16,11 +16,11 @@ end
 if "LYNNUX-PC" == utils.system_name()
    and "lynnux" == utils.user_real_login_name()
    and MajorVersion == 6
-   and MinorVersion == 2
-   and BuildNumber == 9200
+   and MinorVersion == 1
+   and BuildNumber == 7601
 then
-   emacsclientw = "D:\\green\\emacs-25.3-x86_64\\bin\\emacsclientw.exe"
-   runemacs = "D:\\green\\emacs-25.3-x86_64\\bin\\runemacs.exe"
+   emacsclientw = "D:\\green\\emacs-w64-25.3-O2-with-modules\\emacs\\bin\\emacsclientw.exe"
+   runemacs = "D:\\green\\emacs-w64-25.3-O2-with-modules\\emacs\\bin\\runemacs.exe"
 end
 
 -- work pc
