599ec0a9b28042dc94afe658f73fc698c8b0a5a3
lynnux <lynnux@qq.com>
2020-06-03 14:10:18 +0800

    add magit!

--- settings/package_extra.el
+++ settings/package_extra.el
@@ -1,4 +1,4 @@
-;; Time-stamp: <2020-03-03 13:59:55 lynnux>
+;; Time-stamp: <2020-06-03 13:51:46 lynnux>
 ;; 非官方自带packages的设置
 ;; benchmark: 使用profiler-start和profiler-report来查看会影响emacs性能，如造成卡顿的命令等
 ;; 一般都是eldoc会卡，如ggtag和racer mode都是因为调用了其它进程造成卡的
@@ -979,5 +979,9 @@ Copy Buffer Name: _f_ull, _d_irectoy, n_a_me ?
 (autoload 'go-mode "go-mode" nil t)
 (add-to-list 'auto-mode-alist (cons "\\.go\\'" 'go-mode))
 ;; (with-eval-after-load 'go-mode (add-hook 'before-save-hook 'gofmt-before-save))
+
+;; magit
+(add-to-list 'load-path "~/.emacs.d/packages/magit/magit-master/lisp")
+(autoload 'magit "magit" nil t)
 
 (load-theme 'zenburn t)
