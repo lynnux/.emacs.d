e28865e5752f65540533e1b75b86643aef4c27a5
lynnux <lynnux@qq.com>
2021-10-31 19:18:54 +0800

    家里也用上gccemacs，可惜不支持doom emacs呀

--- bin/emacs-win32-launcher.lua
+++ bin/emacs-win32-launcher.lua
@@ -1,17 +1,7 @@
+-- emacs-win32-launcher --debug to see info
 MajorVersion, MinorVersion, BuildNumber = utils.w32_version()
 emacsclientw = ""
 runemacs = ""
-
--- 
-if "20100910-1853" == utils.system_name()
-   and "Administrator" == utils.user_real_login_name()
-   and MajorVersion == 5
-   and MinorVersion == 1
-   and BuildNumber == 2600
-then
-   emacsclientw = "D:\\green\\emacs-24.3\\bin\\emacsclientw.exe"
-   runemacs = "D:\\green\\emacs-24.3\\bin\\runemacs.exe"
-end
 
 if "LYNNUX-PC" == utils.system_name()
    and "lynnux" == utils.user_real_login_name()
@@ -19,8 +9,8 @@ if "LYNNUX-PC" == utils.system_name()
    and MinorVersion == 1
    and BuildNumber == 7601
 then
-   emacsclientw = "D:\\green\\emacs-26.3-x86_64\\bin\\emacsclientw.exe"
-   runemacs = "D:\\green\\emacs-26.3-x86_64\\bin\\runemacs.exe"
+   emacsclientw = "D:\\green\\emacs\\gccemacs\\bin\\emacsclientw.exe"
+   runemacs = "D:\\green\\emacs\\gccemacs\\bin\\runemacs.exe"
 end
 
 -- work pc
