0b8229e421d97b666ca7817b7b8bbc73de574bf0
lynnux <lynnux@qq.com>
2021-11-01 17:09:28 +0800

    还原换行设置

--- settings/gui.el
+++ settings/gui.el
@@ -1,4 +1,4 @@
-;; Time-stamp: <2021-10-31 19:01:17 lynnux>
+;; Time-stamp: <2021-11-01 17:08:43 lynnux>
 ;; 界面相关的
 
 (custom-set-variables
@@ -14,11 +14,11 @@
 
 ;; (setq-default line-spacing 1) ; 这个设置会导致F3到下一个时滚动出问题
 
-;; 二进制文件不能用wrap不然会卡
-(setq-default word-wrap t)
-(setq-default truncate-lines t)
-(setq truncate-partial-width-windows nil)
 ;; (global-visual-line-mode 1); 折行但是不显示两边难看的小符号
+(setq-default word-wrap t)	    ; 换行不打断单词
+;; (setq-default truncate-lines t)
+;; (setq truncate-partial-width-windows nil)
+;; (add-hook 'text-mode-hook #'visual-line-mode)
 
 (set-scroll-bar-mode 'right); 滚动条在右侧(ubuntu)
 
