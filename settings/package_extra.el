720f5b977eebab1e8476ccaef0f1c3fb3f275324
lynnux <lynnux@qq.com>
2022-08-04 11:23:01 +0800

    修复embark里x（consult-file-externally）不出现的bug

--- settings/package_extra.el
+++ settings/package_extra.el
@@ -1440,12 +1440,11 @@ _q_uit
                                                            (interactive "f")
                                                            (browse-file-in-explorer file)))
              )
-           ;; Consult users will also want the embark-consult package.
+           
+           ;; 会绑定consult-file-externally到embark里
            (use-package embark-consult
-             :after (embark consult)
-             :demand t            ; only necessary if you have the hook below
-             ;; if you want to have consult previews as you move around an
-             ;; auto-updating embark collect buffer
+             :after (embark)
+             :demand t
              :hook
              (embark-collect-mode . consult-preview-at-point-mode))
            ;; 随时选择路径
