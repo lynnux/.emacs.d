355b8aacd49d0b6747017398855a35609001ca66
lynnux <lynnux@qq.com>
2022-08-15 10:17:39 +0800

    测试2

--- settings/package_extra.el
+++ settings/package_extra.el
@@ -2515,12 +2515,45 @@ _q_uit
                                             (setq diff-hl-update-timer
                                                   (run-with-idle-timer 2 nil #'diff-hl-update-timer-function) ))
                                           ) nil t)
-                              (add-hook 'magit-post-commit-hook '(lambda()
-                                                                   (let ((diff-hl-mode t))
-                                                                     (diff-hl-magit-post-refresh))
-                                                                   ))
                               ))
   :config
+  (with-eval-after-load 'magit
+    (add-hook 'magit-post-commit-hook 'my-diff-hl-magit-post-refresh)
+    ;; 貌似只有这个处理buffer-list
+    (defun my-diff-hl-magit-post-refresh ()
+      (unless (and diff-hl-disable-on-remote
+                   (file-remote-p default-directory))
+        (let* ((topdir (magit-toplevel))
+               (modified-files
+                (mapcar (lambda (file) (expand-file-name file topdir))
+                        (delete-consecutive-dups
+                         (sort
+                          (nconc (magit-unstaged-files t)
+                                 diff-hl--magit-unstaged-files)
+                          #'string<))))
+               (unmodified-states '(up-to-date ignored unregistered)))
+          (setq diff-hl--magit-unstaged-files nil)
+          (dolist (buf (buffer-list))
+            (when (and (not (buffer-modified-p buf))
+                       ;; Solve the "cloned indirect buffer" problem
+                       ;; (diff-hl-mode could be non-nil there, even if
+                       ;; buffer-file-name is nil):
+                       (buffer-file-name buf)
+                       (file-in-directory-p (buffer-file-name buf) topdir)
+                       (file-exists-p (buffer-file-name buf)))
+              (with-current-buffer buf
+                (let* ((file buffer-file-name)
+                       (backend (vc-backend file)))
+                  (when backend
+                    (cond
+                     ((member file modified-files)
+                      (when (memq (vc-state file) unmodified-states)
+                        (vc-state-refresh file backend))
+                      (diff-hl-update))
+                     ((not (memq (vc-state file backend) unmodified-states))
+                      (vc-state-refresh file backend)
+                      (diff-hl-update)))))))))))
+    )
   ;; 用timer避免各种hook
   (defvar diff-hl-update-timer nil)
   (defun diff-hl-update-timer-function()
