62d6a45060674866b4be20862f0d676d6ba874a0
lynnux <lynnux@qq.com>
2022-09-19 11:47:34 +0800

    update orderless same to minmad

--- settings/package_extra.el
+++ settings/package_extra.el
@@ -1431,26 +1431,29 @@ _c_: hide comment        _q_uit
           (?= . orderless-literal) ;; 纯文本包含(regexp-quote实现)
           (?~ . orderless-flex)) ;; fuzzy，不过是有顺序的
         )
-      (defun +orderless-dispatch (pattern index _total)
+      (defun +orderless--suffix-regexp ()
+        (if (and (boundp 'consult--tofu-char) (boundp 'consult--tofu-range))
+            (format "[%c-%c]*$"
+                    consult--tofu-char
+                    (+ consult--tofu-char consult--tofu-range -1))
+          "$"))
+      (defun +orderless-dispatch (word _index _total)
         (cond
          ;; Ensure that $ works with Consult commands, which add disambiguation suffixes
-         ((string-suffix-p "$" pattern)
-          `(orderless-regexp . ,(concat (substring pattern 0 -1) "[\x200000-\x300000]*$")))
+         ((string-suffix-p "$" word)
+          `(orderless-regexp . ,(concat (substring word 0 -1) (+orderless--suffix-regexp))))
          ;; File extensions
-         ((and
-           ;; Completing filename or eshell
-           (or minibuffer-completing-file-name
-               (derived-mode-p 'eshell-mode))
-           ;; File extension
-           (string-match-p "\\`\\.." pattern))
-          `(orderless-regexp . ,(concat "\\." (substring pattern 1) "[\x200000-\x300000]*$")))
+         ((and (or minibuffer-completing-file-name
+                   (derived-mode-p 'eshell-mode))
+               (string-match-p "\\`\\.." word))
+          `(orderless-regexp . ,(concat "\\." (substring word 1) (+orderless--suffix-regexp))))
          ;; Ignore single !
-         ((string= "!" pattern) `(orderless-literal . ""))
+         ((equal "!" word) `(orderless-literal . ""))
          ;; Prefix and suffix
-         ((if-let (x (assq (aref pattern 0) +orderless-dispatch-alist))
-              (cons (cdr x) (substring pattern 1))
-            (when-let (x (assq (aref pattern (1- (length pattern))) +orderless-dispatch-alist))
-              (cons (cdr x) (substring pattern 0 -1)))))))
+         ((if-let (x (assq (aref word 0) +orderless-dispatch-alist))
+              (cons (cdr x) (substring word 1))
+            (when-let (x (assq (aref word (1- (length word))) +orderless-dispatch-alist))
+              (cons (cdr x) (substring word 0 -1)))))))
       ;; +orderless-with-initialism直接进completion-styles-alist了
       (orderless-define-completion-style +orderless-with-initialism
         (orderless-matching-styles '(orderless-initialism orderless-literal orderless-regexp)))
@@ -1461,7 +1464,7 @@ _c_: hide comment        _q_uit
       (add-hook 'emacs-lisp-mode-hook (lambda ()
                                         (make-local-variable 'orderless-style-dispatchers)
                                         (setq orderless-style-dispatchers '(+orderless-dispatch my/orderless-dispatch-flex-first))))
-      (setq completion-styles '(orderless partial-completion)
+      (setq completion-styles '(orderless basic)
             completion-category-defaults nil
             completion-category-overrides '((file (styles flex)) ;; helm是flex
                                             (command (styles +orderless-with-initialism)) ; 相当于ivy的^吧？
