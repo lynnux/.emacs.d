74bb1f56950eccafb042512f1475bc9cfbd02761
lynnux <lynnux@qq.com>
2023-08-03 16:47:07 +0800

    使用最新版本emacs

--- bin/emacs-win32-launcher.lua
+++ bin/emacs-win32-launcher.lua
@@ -19,8 +19,8 @@ if "DESKTOP-9JD2LQI" == utils.system_name()
    and MinorVersion == 2
    and BuildNumber == 9200
 then
-   emacsclientw = "c:\\green\\Emacs\\gccemacs_230627\\bin\\emacsclientw.exe"
-   runemacs = "c:\\green\\Emacs\\gccemacs_230627\\bin\\runemacs.exe"
+   emacsclientw = "c:\\green\\Emacs\\gccemacs_230803\\bin\\emacsclientw.exe"
+   runemacs = "c:\\green\\Emacs\\gccemacs_230803\\bin\\runemacs.exe"
 end
 
 -- get commandline
